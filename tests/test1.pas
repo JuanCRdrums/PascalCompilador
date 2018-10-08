68d3a5d012b25910dd296c6444e833cc287721ef
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-10-08 01:45:48 -0500

    Archivo de prueba completo.

--- tests/test1.pas
+++ tests/test1.pas
@@ -7,6 +7,20 @@ begin
   read(x,y,z);
   write(x,y,x+y);
   read(y);
-  if x = y then
+  if -x <> y*1 then
     write(x)
+  else
+    write(y);
+  read(z);
+  if not "holi" = (y and 1 = 2) then
+  begin
+    write("hola");
+    write("chao")
+  end
+  else
+  begin
+    read(z);
+    y := x + 1
+  end;
+  read(x)
 end.
